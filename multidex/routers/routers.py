e5ffd9923af3df823e9fe2527f0b5e6576a276f3
m-stclair <mstclair@millionconcepts.com>
2021-06-04 03:02:36 -0400

    tidying

--- multidex/routers/routers.py
+++ multidex/routers/routers.py
@@ -27,10 +27,3 @@ class InstrumentRouter:
         if instrument_or_none(model1) == instrument_or_none(model2):
             return True
         return None
-
-    #
-    # def allow_migrate(self, db, app_label, model_name=None, **hints):
-    #     if app_label in self.route_app_labels:
-    #         return db == 'auth_db'
-    #     return None
-    #
