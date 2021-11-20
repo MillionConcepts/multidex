35259ba9feb431785aa6c60ad3ef4a64b84b1b9f
mstclair@millionconcepts.com <mstclair@millionconcepts.com>
2021-11-20 14:30:25 -0500

    merge thumbpipe:

--- multidex/ingest/zcam.py
+++ multidex/ingest/zcam.py
@@ -8,7 +8,10 @@ import numpy as np
 from fs.osfs import OSFS
 import pandas as pd
 
-from zcam_thumbpipe import default_thumbnailer
+import io
+
+from PIL import Image
+from dustgoggles.composition import Composition
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multidex.settings")
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
@@ -16,6 +19,52 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
 
 from plotter.models import ZSpec
+
+
+# make consistently-sized thumbnails out of the asdf context images. we
+# might eventually want a type of output earmarked for this, or to write the
+# online thumbnails out locally. (something like this can be inserted as a
+# send into look pipelines...?)
+
+def remove_alpha(image):
+    return image.convert("RGB")
+
+
+def pil_crop(image, bounds):
+    bounds = (
+        bounds[0],
+        bounds[3],
+        image.size[0] - bounds[1],
+        image.size[1] - bounds[2],
+    )
+    cropped = image.crop(bounds)
+    return cropped
+
+
+def thumber(image, scale):
+    image.thumbnail((image.size[0] / scale, image.size[1] / scale))
+    return image
+
+
+def jpeg_buffer(image):
+    buffer = io.BytesIO()
+    image.save(buffer, "jpeg")
+    buffer.seek(0)
+    return buffer
+
+
+def default_thumbnailer():
+    params = {"crop": {"bounds": (20, 20, 122, 5)}, "thumb": {"scale": 2}}
+    steps = {
+        "load": Image.open,
+        "flatten": remove_alpha,
+        "crop": pil_crop,
+        "thumb": thumber,
+        "write": jpeg_buffer,
+    }
+    return Composition(steps=steps, parameters=params)
+
+
 
 ASDF_STEM_PATTERN = re.compile(r"SOL\d{4}_zcam\d{5}_RSM\d{1,4}")
 ASDF_LEGACY_STEM_PATTERN = re.compile(
