7a513e3904bb9748598c92472d47984c8bc4f598
Colleen O'Shea <colleen.oshea@jhuapl.edu>
2022-04-05 13:46:31 -0400

    add images to ingest process

--- multidex/ingest/ccam/cli.py
+++ multidex/ingest/ccam/cli.py
@@ -1,6 +1,4 @@
-import datetime as dt
 import os
-import re
 from operator import attrgetter
 from pathlib import Path
 
@@ -19,14 +17,14 @@ from plotter.models import CSpec
 CSPEC_FIELD_NAMES = list(map(attrgetter("name"), CSpec._meta.fields))
 
 
-def looks_like_marslab(fn: str) -> bool:
+def looks_like_cspec(fn: str) -> bool:
     return bool(
-        ("marslab" in fn) and not ("extended" in fn) and (fn.endswith(".csv"))
+        ("cspec" in fn) and not ("extended" in fn) and (fn.endswith(".csv"))
     )
 
 
 def looks_like_context(fn: str) -> bool:
-    return fn.endswith(".png") and ("context" in fn) and ("pixmap" not in fn)
+    return fn.endswith(".png") and ("ccam" in fn) and ("pixmap" not in fn)
 
 
 def directory_of(path: Path) -> str:
@@ -39,13 +37,13 @@ def find_ingest_files(path: Path, recursive: bool = False):
     if recursive:
         tree = OSFS(directory_of(path))
         marslab_files = map(
-            tree.getsyspath, filter(looks_like_marslab, tree.walk.files())
+            tree.getsyspath, filter(looks_like_cspec, tree.walk.files())
         )
         context_files = map(
             tree.getsyspath, filter(looks_like_context, tree.walk.files())
         )
     elif path.is_dir():
-        marslab_files = filter(looks_like_marslab, map(str, path.iterdir()))
+        marslab_files = filter(looks_like_cspec, map(str, path.iterdir()))
         context_files = filter(looks_like_context, map(str, path.iterdir()))
     else:
         marslab_files = [str(path)]
@@ -66,28 +64,40 @@ CCAM_BOOL_FIELDS = [
 
 def process_context_files(context_files):
     context_df = pd.DataFrame(context_files, columns=["path"])
-    context_df["stem"] = [
-        Path(path).name for path in context_df["path"].apply(asdf_stemmer)
-    ]
-    context_df["eye"] = context_df["path"].str.extract(r"(left|right)")
     context_df["save"] = False
     return context_df
 
 
-def process_marslab_row(row, marslab_file):
+def process_marslab_row(row, marslab_file, context_df):
     row = row.dropna()
     relevant_indices = [ix for ix in row.index if ix in CSPEC_FIELD_NAMES]
     for filt in set(CSpec.filters).intersection(row.index):
         row[filt] = float(row[filt])
+
+    # see if there is a matching image
+    if context_df is not None:
+        sol = row['sol']
+        seq_id = row['seq_id']
+        img_file = '{sol:04d}_crm_{seq_id}'.format(sol=sol, seq_id=seq_id)
+        context_matches = context_df.loc[context_df['path'].str.contains(img_file, case=False)]
+        obs_image = None
+
+        if len(context_matches.index) > 1:
+            print("more than one!")
+        if not context_matches.empty:
+            for record in context_matches[["path"]].to_dict(orient="records"):
+                obs_image = os.path.basename(record["path"])
+
     metadata = dict(row[relevant_indices]) | {
         "filename": Path(marslab_file).name,
-        "images": [],
+        "images": [obs_image],
         # "ingest_time": dt.datetime.utcnow().isoformat()[:-7] + "Z",
         # "min_count": row[row.index.str.contains("count")].astype(float).min(),
     }
+
     # fix negative distances from darks
-    if metadata['distance_m'] < 0:
-        metadata['distance_m'] = 5000
+    if metadata['target_distance'] < 0:
+        metadata['target_distance'] = 5000
     # convert all target types to lower case
     metadata['target_type_shot_specific'] = str.lower(metadata['target_type_shot_specific'])
     # get sclk from filename
@@ -114,23 +124,23 @@ def format_for_multidex(frame):
     return frame
 
 
-def ingest_marslab_file(marslab_file, context_df):
-    frame = pd.read_csv(marslab_file)
+def ingest_cspec_file(cspec_file, context_df):
+    frame = pd.read_csv(cspec_file)
     if frame["INSTRUMENT"].iloc[0] != "CCAM":
-        print("skipping non-CCAM file: " + marslab_file)
+        print("skipping non-CCAM file: " + cspec_file)
         return False, context_df
-    print("ingesting spectra from " + Path(marslab_file).name)
+    print("ingesting spectra from " + Path(cspec_file).name)
     # regularize various ways people may have rendered metadata fields
     try:
         frame = format_for_multidex(frame)
     except KeyboardInterrupt:
         raise
     except Exception as ex:
-        print("failed on " + marslab_file + " reformat: " + str(ex))
+        print("failed on " + cspec_file + " reformat: " + str(ex))
         return False, context_df
     colors = []
     for _, row in frame.iterrows():
-        row_color = process_marslab_row(row, marslab_file)
+        row_color = process_marslab_row(row, cspec_file, context_df)
         if row_color is not None:
             colors.append(row_color)
     print("successfully ingested " + ", ".join(colors))
@@ -141,23 +151,26 @@ def ingest_multidex(
     path_or_file, *, recursive: "r" = False, skip_thumbnails: "t" = False
 ):
     """
-    ingests zcam -marslab.csv files and context image thumbnails generated
-    by asdf into a multidex database. expects all products in an observation
+    ingests ccam -cspec.csv files and context image thumbnails
+    into a multidex database. expects all products in an observation
     to have matching filenames -- if you modify the filenames output by
     asdf or related applications, this script will likely fail, although you
     might be able to convince it if you really try.
 
-    param path_or_file: marslab file or directory containing marslab files.
+    param path_or_file: cspec file or directory containing cspec files.
         looks for matching context images within the same directory.
-    param recursive: attempts to ingest all marslab files and context images
+    param recursive: attempts to ingest all cspec files and context images
         in directory tree, regardless of what specific file you passed it
     param skip_thumbnails: don't process context images or make thumbnails.
     """
     path = Path(path_or_file)
-    marslab_files, context_files = find_ingest_files(path, recursive)
-    context_df = None
-    successfully_ingested_marslab_files = []
-    for marslab_file in marslab_files:
-        successful, context_df = ingest_marslab_file(marslab_file, context_df)
+    cspec_files, context_files = find_ingest_files(path, recursive)
+    if not skip_thumbnails:
+        context_df = process_context_files(context_files)
+    else:
+        context_df = None
+    successfully_ingested_cspec_files = []
+    for cspec_file in cspec_files:
+        successful, context_df = ingest_cspec_file(cspec_file, context_df)
         if successful:
-            successfully_ingested_marslab_files.append(marslab_file)
+            successfully_ingested_cspec_files.append(cspec_file)
