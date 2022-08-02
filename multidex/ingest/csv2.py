52bb851b28ac192698e1fa208bdbca81a334114e
michael <mstclair@millionconcepts.com>
2022-08-02 12:45:45 -0400

    add quiet option to csv ingest

--- multidex/ingest/csv2.py
+++ multidex/ingest/csv2.py
@@ -2,7 +2,6 @@
 simple tools to ingest simulated spectra produced by VISOR.
 does not handle images at the moment.
 """
-
 import os
 
 import django
@@ -19,7 +18,7 @@ from plotter.models import INSTRUMENT_MODEL_MAPPING
 
 # TODO: how do I track full file provenance? It would be great if there were
 #  a PRODUCT_ID or similar for the original CSVs...
-def perform_ingest(csv_fn, instrument_code, dry_run=False):
+def perform_ingest(csv_fn, instrument_code, *, dry_run=False, quiet=False):
     frame = pd.read_csv(csv_fn)
     if "UNITS" in frame.columns:
         if "R*" in frame["UNITS"].values:
@@ -49,15 +48,16 @@ def perform_ingest(csv_fn, instrument_code, dry_run=False):
         print("******** DRY RUN ONLY *********")
     for ix, row in frame.iterrows():
         try:
+            row = row.replace('-', float('nan')).dropna().to_dict()
             spectrum = model(
-                **{
-                    key.lower(): value
-                    for key, value in row.dropna().to_dict().items()
-                }
+                **{key.lower(): value for key, value in row.items()}
             )
             spectrum.clean()
             if dry_run is not True:
                 spectrum.save()
-            print(f"ingested {modeldict(spectrum)}")
+            if quiet is False:
+                print(f"ingested {modeldict(spectrum)}")
+        except KeyboardInterrupt:
+            raise
         except Exception as ex:
             print(f"failed on row {ix}: {type(ex)}: {ex}")
