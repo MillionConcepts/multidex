f8f99b2d33a04b6357483ebe8a68916dd316334c
m-stclair <mstclair@millionconcepts.com>
2025-06-02 16:39:19 -0400

    revert unintended change to user_output_style

--- multidex/plotter/config/user_output_style.py
+++ multidex/plotter/config/user_output_style.py
@@ -5,6 +5,6 @@ output_style at runtime.
 
 # FONT_PATH = "/path/to/my/font/file"
 # FONT_PATH_BOLD = "/path/to/my/bold/font/file"
-LABEL_TEXT_SIZE = 80
-TICK_TEXT_SIZE = 80
-FITLINE_TEXT_SIZE = 80
+# LABEL_TEXT_SIZE = 30
+# TICK_TEXT_SIZE = 24
+# FITLINE_TEXT_SIZE = 26
